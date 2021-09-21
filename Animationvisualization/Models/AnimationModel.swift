c8e37cb20bcec4906022db07465965a10964552c
Nadzeya Shpakouskaya <shpakovna@gmail.com>
2021-09-21 11:47:14 +0400

    Create AnimationModel, create DataManager

--- Animationvisualization/Models/AnimationModel.swift
+++ Animationvisualization/Models/AnimationModel.swift
@@ -5,8 +5,92 @@
 //  Created by Nadzeya Shpakouskaya on 21/09/2021.
 //
 
-import Foundation
-
-struct AnimationModel{
+struct AnimationModel {
+    let preset: Preset
+    let curve: Curve
+    let duration: Double
+    let delay: Double
+    let force: Double
     
+    static func generateAnimations(counts: Int) -> [AnimationModel] {
+        var animations: [AnimationModel] = []
+        for _ in 1...counts {
+            let preset = Preset.allCases.randomElement()
+            let curve = Curve.allCases.randomElement()
+            let duration = Double.random(in: 0.5...2)
+            let delay = Double.random(in: 0.35...1)
+            let force = Double.random(in: 0.5...1.5)
+            let animation = AnimationModel(
+                preset: preset ?? .fadeIn,
+                curve: curve ?? .easeInOut,
+                duration: duration,
+                delay: delay,
+                force: force)
+            animations.append(animation)
+        }
+        
+        return animations
+    }
+}
+
+enum Preset: String, CaseIterable {
+    case slideLeft
+    case slideRight
+    case slideDown
+    case slideUp
+    case squeezeLeft
+    case squeezeRight
+    case squeezeDown
+    case squeezeUp
+    case fadeIn
+    case fadeOut
+    case fadeOutIn
+    case fadeInLeft
+    case fadeInRight
+    case fadeInDown
+    case fadeInUp
+    case zoomIn
+    case zoomOut
+    case fall
+    case shake
+    case pop
+    case flipX
+    case flipY
+    case morph
+    case squeeze
+    case flash
+    case wobble
+    case swing
+}
+
+enum Curve: String, CaseIterable {
+    case easeIn
+    case easeOut
+    case easeInOut
+    case linear
+    case spring
+    case easeInSine
+    case easeOutSine
+    case easeInOutSine
+    case easeInQuad
+    case easeOutQuad
+    case easeInOutQuad
+    case easeInCubic
+    case easeOutCubic
+    case easeInOutCubic
+    case easeInQuart
+    case easeOutQuart
+    case easeInOutQuart
+    case easeInQuint
+    case easeOutQuint
+    case easeInOutQuint
+    case easeInExpo
+    case easeOutExpo
+    case easeInOutExpo
+    case easeInCirc
+    case easeOutCirc
+    case easeInOutCirc
+    case easeInBack
+    case easeOutBack
+    case easeInOutBack
 }
